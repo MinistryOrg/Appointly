3ba7eae7399396e5ee9c8767bda0d7f036f35cfd
root <pgeo82@gmail.com>
2023-11-08 09:47:16 +0200

    first commit

---
+++ src/test/java/com/mom/appointly/AppointlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mom.appointly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
