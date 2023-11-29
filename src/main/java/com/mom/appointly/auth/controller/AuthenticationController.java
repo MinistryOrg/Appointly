ea0305a363e30e0eea55782ab72669e9c4d2ac35
root <pgeo82@gmail.com>
2023-11-29 13:08:14 +0200

    add cross origin in controller

--- src/main/java/com/mom/appointly/auth/controller/AuthenticationController.java
+++ src/main/java/com/mom/appointly/auth/controller/AuthenticationController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/auth/appointly")
 @RequiredArgsConstructor
 public class AuthenticationController {
