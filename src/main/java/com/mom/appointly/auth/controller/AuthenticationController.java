c942fa7492a004bbc90077a3f376c1164decf276
root <pgeo82@gmail.com>
2024-01-05 14:51:07 +0200

    search the shops without login

--- src/main/java/com/mom/appointly/auth/controller/AuthenticationController.java
+++ src/main/java/com/mom/appointly/auth/controller/AuthenticationController.java
@@ -1,11 +1,14 @@
 package com.mom.appointly.auth.controller;
 
+import com.mom.appointly.AppointlyApplication;
 import com.mom.appointly.auth.model.AuthenticationRequest;
 import com.mom.appointly.auth.model.AuthenticationResponse;
 import com.mom.appointly.auth.model.ChangePasswordRequest;
 import com.mom.appointly.auth.model.RegisterRequest;
 import com.mom.appointly.auth.service.AuthenticationService;
+import com.mom.appointly.service.AppointlyService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +20,7 @@ import java.security.Principal;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+    private final AppointlyService appointlyService;
 
     @GetMapping("/test")
     public  ResponseEntity<?> getTest(){
@@ -45,6 +49,10 @@ public class AuthenticationController {
     @GetMapping("/logout")
     public void logout() {
         SecurityContextHolder.clearContext();
+    }
+    @GetMapping("/shopsByLocationService")
+    private ResponseEntity<?> getByLocationService(@RequestParam String location, @RequestParam String service){
+        return new ResponseEntity<>(appointlyService.searchByLocationAndService(location, service), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/mom/appointly/auth/service/AuthenticationService.java
+++ src/main/java/com/mom/appointly/auth/service/AuthenticationService.java
@@ -74,4 +74,6 @@ public class AuthenticationService {
         user.setPassword(passwordEncoder.encode(changePasswordRequest.getNewPassword()));
         userRepo.save(user);
     }
+
+
 }
